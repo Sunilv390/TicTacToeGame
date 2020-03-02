12dffdd369d559d58c9d0309771dc27ce3214a6e
Sunilv390 <sunilv390@gmail.com>
2020-03-02 09:08:57 +0530

    Added: Reseting the board

--- ticTactoe.sh
+++ ticTactoe.sh
@@ -1,3 +1,19 @@
-#!/bin/bash -x
+#!/bin/bash
 
-echo *************Welcome******************
+#CONSTANTS
+row=3
+column=3
+
+declare -A board
+#RESETTING THE BOARD
+function resetBoard(){
+	local i=0
+	local j=0
+	for (( i=0; i<$row; i++ ))
+	do
+		for (( j=0; j<$column; j++ ))
+		do
+			board[$i,$j]=0
+		done
+	done
+}
